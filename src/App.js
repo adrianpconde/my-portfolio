226a08929443d6c16fab537fb695aa9ca038d48c
Adrian Perez <adrian.perez.conde22@gmail.com>
2022-11-10 15:24:47 +0100

    feat: linkedin and github anchors added and some style changes

--- src/App.js
+++ src/App.js
@@ -2,19 +2,18 @@ import "./App.scss";
 import Topbar from "./components/top-bar/Topbar";
 import Intro from "./components/intro/Intro";
 import Portfolio from "./components/portfolio/Portfolio";
-import Resume from "./components/resume/Resume"
+import Resume from "./components/resume/Resume";
 import Contact from "./components/contact/Contact";
 import { useState } from "react";
 import Menu from "./components/menu/Menu";
 
 function App() {
-  const [menuOpen, setMenuOpen] = useState(false)
-
+  const [menuOpen, setMenuOpen] = useState(false);
 
   return (
     <div className="App">
-      <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
-      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+      <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       <div className="sections">
         <Intro />
         <Portfolio />
--- src/components/top-bar/Topbar.jsx
+++ src/components/top-bar/Topbar.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import "./topbar.scss";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import MailIcon from "@mui/icons-material/Mail";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
-function Topbar({ menuOpen, setMenuOpen }) {
+function Topbar({ menuOpen, setMenuOpen, theme, toggleTheme }) {
   return (
     <div className={"topbar " + (menuOpen && "active")}>
       <div className="wrapper">
@@ -19,6 +21,19 @@ function Topbar({ menuOpen, setMenuOpen }) {
           <div className="itemContainer">
             <MailIcon className="icon" />
             <span>adrian.perez.conde22@gmail.com</span>
+          </div>
+          <div className="itemContainer">
+            <a href="https://www.linkedin.com/in/adrianpconde/">
+              <LinkedInIcon className="icon" />
+              <span>My LinkedIn Profile</span>
+            </a>
+          </div>
+          <div className="itemContainer">
+            <a href="https://github.com/adrianpconde">
+              <GitHubIcon className="icon" />
+              <span>https://github.com/adrianpconde</span>
+
+            </a>
           </div>
         </div>
         <div className="right">
